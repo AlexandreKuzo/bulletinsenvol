47f1f6f0602b3065524b005062858b32156ec5fa
Alexandre Kuzo <42785933+AlexandreKuzo@users.noreply.github.com>
2019-07-18 17:55:54 +0200

    Update relationships ; User.php

--- app/User.php
+++ app/User.php
@@ -26,4 +26,52 @@ class User extends Authenticatable {
 		'password', 'remember_token',
 	];
 
+	public function bulletins() {
+		return $this->hasMany(Bulletin::class , 'email', 'email');
+		return $this->hasMany(Bulletin::class , 'Trimestre', 'Trimestre');
+		return $this->hasMany(Bulletin::class , 'created_at', 'created_at');
+
+	}
+
+	public function secondes() {
+		return $this->hasMany(Seconde::class , 'email', 'email');
+		return $this->hasMany(Seconde::class , 'created_at', 'created_at');
+
+	}
+
+	public function scientifiques() {
+		return $this->hasMany(Scientifique::class , 'email', 'email');
+		return $this->hasMany(Scientifique::class , 'Trimestre', 'Trimestre');
+		return $this->hasMany(Scientifique::class , 'created_at', 'created_at');
+
+	}
+
+	public function economiques() {
+		return $this->hasMany(Economique::class , 'email', 'email');
+		return $this->hasMany(Economique::class , 'Trimestre', 'Trimestre');
+		return $this->hasMany(Ecnonomique::class , 'created_at', 'created_at');
+
+	}
+
+	public function litteraires() {
+		return $this->hasMany(Litteraire::class , 'email', 'email');
+		return $this->hasMany(Litteraire::class , 'Trimestre', 'Trimestre');
+		return $this->hasMany(Litteraire::class , 'created_at', 'created_at');
+
+	}
+
+	public function sciencestechniques() {
+		return $this->hasMany(Sciencetechnique::class , 'email', 'email');
+		return $this->hasMany(Sciencetechnique::class , 'Trimestre', 'Trimestre');
+		return $this->hasMany(Sciencetechnique::class , 'created_at', 'created_at');
+
+	}
+
+	public function sciencesgestions() {
+		return $this->hasMany(Sciencegestion::class , 'email', 'email');
+		return $this->hasMany(Sciencegestion::class , 'Trimestre', 'Trimestre');
+		return $this->hasMany(Sciencegestion::class , 'created_at', 'created_at');
+
+	}
+
 }
